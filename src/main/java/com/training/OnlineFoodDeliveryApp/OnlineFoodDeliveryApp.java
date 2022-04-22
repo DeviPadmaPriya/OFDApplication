6c9718893ff87717546554dc0031696bf77b64b0
ELCOT <ELCOT@Lenovo>
2022-04-22 10:21:34 +0530

    project

---
+++ src/main/java/com/training/OnlineFoodDeliveryApp/OnlineFoodDeliveryApp.java
@@ -0,0 +1,13 @@
+package com.training.OnlineFoodDeliveryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodDeliveryApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodDeliveryApp.class, args);
+	}
+
+}
